ab3fc8615b01d0139ca2bd2a8071cfc3980608ef
Ben Shearer <ben.shearer1994@gmail.com>
2021-07-10 09:55:02 +0100

    Add factories

---
+++ src/factory/activity-type-defaults.js
@@ -0,0 +1,7 @@
+export function ActivityTypeDefaults() {
+  return [
+    { title: "Health & Fitness", uid: "health" },
+    { title: "Hobbies & Interests", uid: "hobbies" },
+    { title: "Productivity & Learning", uid: "productivity" },
+  ];
+}
---
+++ src/factory/dialog-styles.js
@@ -0,0 +1,18 @@
+export function DeleteDialogStyle() {
+  return {
+    content: {
+      background: "#2a4d69",
+      bottom: "auto",
+      boxShadow: "0rem 0rem 0.2rem 0 #2a4d69",
+      color: "#f9fcff",
+      left: "50%",
+      marginRight: "-50%",
+      padding: "2rem",
+      position: "fixed",
+      right: "auto",
+      top: "50%",
+      transform: "translate(-50%, -50%)",
+    },
+    overlay: { zIndex: 10000 },
+  };
+}
