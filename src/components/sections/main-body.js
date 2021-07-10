0b9e95aebc55b1fdb347c1a9831591ae0f632aed
Ben Shearer <ben.shearer1994@gmail.com>
2021-07-10 09:53:45 +0100

    Add main body

---
+++ src/components/sections/main-body.js
@@ -0,0 +1,13 @@
+import MainCalendar from "../calendars/main-calendar";
+import MainPanel from "../panels/main-panel/main-panel";
+import styles from "./main-body.module.css";
+
+export default function MainBody() {
+  return (
+    <div className={styles["main-body"]} data-testid="main-body">
+      <MainCalendar />
+
+      <MainPanel />
+    </div>
+  );
+}
